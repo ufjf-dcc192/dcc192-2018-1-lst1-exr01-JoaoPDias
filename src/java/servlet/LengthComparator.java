3c98bef6c70fdb03080c65e2f89e0fd7f64de463
João Paulo Dias <jpdias1997@hotmail.com>
2018-03-23 22:02:07 -0300

    Métodos de Ordenação por Nome, por Tamanho e Randômico

---
+++ src/java/servlet/LengthComparator.java
@@ -0,0 +1,12 @@
+
+package servlet;
+
+
+public class LengthComparator implements java.util.Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+}  
+
--- src/java/servlet/ListFrutasServlet.java
+++ src/java/servlet/ListFrutasServlet.java
@@ -7,6 +7,9 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,16 +23,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ListFrutasServlet", urlPatterns = {"/listafrutas.html"})
 public class ListFrutasServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    List<String> frutas;
+
+    public ListFrutasServlet() {
+        frutas = new ArrayList<>();
+        frutas.add("damasco");
+        frutas.add("gravi");
+        frutas.add("melancias");
+        frutas.add("jenipapo");
+        frutas.add("uva");
+        frutas.add("maça");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -37,52 +45,35 @@ public class ListFrutasServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ListFrutasServlet</title>");            
+            out.println("<title>Lista de Frutas</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ListFrutasServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Frutas</h1>");
+            out.println("<ul>");
+            String ordenacao = request.getParameter("ordenacao");
+            LengthComparator length = new LengthComparator();
+            switch (ordenacao) {
+                case "ran":
+                    Collections.shuffle(frutas);
+                    break;
+                case "tam":
+                    Collections.sort(frutas, length);
+                    break;
+                case "alfa":
+                    Collections.sort(frutas);
+                    break;
+
+            }
+            for (String fruta : frutas) {
+                out.println("<li>" + fruta + "</li>");
+            }
+            out.println("<a href='listafrutas.html?ordenacao=alfa'>Ordem Alfabética</a> /");
+            out.println("<a href='listafrutas.html?ordenacao=ran'>Ordem Randômica</a> /");
+            out.println("<a href='listafrutas.html?ordenacao=tam'>Ordem por Tamanho</a>)</li>");
+            out.println("</ul>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
